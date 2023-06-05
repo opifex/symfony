b6c8ff944a95035d4365a4c8814f98ae3700aaa0
Mykyta Zinchenko <opifex.ua@gmail.com>
2023-06-05 15:29:40 +0300

    Initial commit

--- src/Domain/Response/GetAccountByIdResponse.php
+++ src/Domain/Response/GetAccountByIdResponse.php
@@ -4,7 +4,42 @@ declare(strict_types=1);
 
 namespace App\Domain\Response;
 
-final class GetAccountByIdResponse extends AbstractAccountResponse
+use App\Domain\Entity\Account;
+use DateTimeImmutable;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+final class GetAccountByIdResponse
 {
     public const GROUP_VIEW = __CLASS__ . ':view';
+
+    #[Groups([self::GROUP_VIEW])]
+    public readonly string $uuid;
+
+    #[Groups([self::GROUP_VIEW])]
+    public readonly string $email;
+
+    #[Groups([self::GROUP_VIEW])]
+    public readonly string $status;
+
+    /**
+     * @var string[]
+     */
+    #[Groups([self::GROUP_VIEW])]
+    public readonly array $roles;
+
+    #[Groups([self::GROUP_VIEW])]
+    public readonly ?DateTimeImmutable $createdAt;
+
+    #[Groups([self::GROUP_VIEW])]
+    public readonly ?DateTimeImmutable $updatedAt;
+
+    public function __construct(Account $account)
+    {
+        $this->createdAt = $account->getCreatedAt();
+        $this->email = $account->getEmail();
+        $this->roles = $account->getRoles();
+        $this->status = $account->getStatus();
+        $this->updatedAt = $account->getUpdatedAt();
+        $this->uuid = $account->getUuid();
+    }
 }
--- src/Domain/Response/GetAccountsByCriteriaItem.php
+++ src/Domain/Response/GetAccountsByCriteriaItem.php
@@ -4,6 +4,40 @@ declare(strict_types=1);
 
 namespace App\Domain\Response;
 
-final class GetAccountsByCriteriaItem extends AbstractAccountResponse
+use App\Domain\Entity\Account;
+use DateTimeImmutable;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+final class GetAccountsByCriteriaItem
 {
+    #[Groups([GetAccountsByCriteriaResponse::GROUP_VIEW])]
+    public readonly string $uuid;
+
+    #[Groups([GetAccountsByCriteriaResponse::GROUP_VIEW])]
+    public readonly string $email;
+
+    #[Groups([GetAccountsByCriteriaResponse::GROUP_VIEW])]
+    public readonly string $status;
+
+    /**
+     * @var string[]
+     */
+    #[Groups([GetAccountsByCriteriaResponse::GROUP_VIEW])]
+    public readonly array $roles;
+
+    #[Groups([GetAccountsByCriteriaResponse::GROUP_VIEW])]
+    public readonly ?DateTimeImmutable $createdAt;
+
+    #[Groups([GetAccountsByCriteriaResponse::GROUP_VIEW])]
+    public readonly ?DateTimeImmutable $updatedAt;
+
+    public function __construct(Account $account)
+    {
+        $this->createdAt = $account->getCreatedAt();
+        $this->email = $account->getEmail();
+        $this->roles = $account->getRoles();
+        $this->status = $account->getStatus();
+        $this->updatedAt = $account->getUpdatedAt();
+        $this->uuid = $account->getUuid();
+    }
 }
