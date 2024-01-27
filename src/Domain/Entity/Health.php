2bdb41c05f5515b55f556d2dc2b599b1cf43faca
Mykyta Zinchenko <opifex.ua@gmail.com>
2024-01-27 09:47:55 +0200

    Initial commit

--- src/Domain/Entity/Health.php
+++ src/Domain/Entity/Health.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Entity;
 
-use App\Domain\Contract\HealthInterface;
-use Override;
-
-class Health implements HealthInterface
+class Health
 {
     public function __construct(private HealthStatus $status)
     {
     }
 
-    #[Override]
     public function getStatus(): HealthStatus
     {
         return $this->status;
