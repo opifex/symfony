3823fa743965bb312cbb68ef29fb613b92c500d6
Mykyta Zinchenko <opifex.ua@gmail.com>
2024-02-19 15:23:37 +0200

    Initial commit

--- src/Domain/Contract/AccountStateMachineInterface.php
+++ src/Domain/Contract/AccountStateMachineInterface.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Domain\Contract;
 
 use App\Domain\Exception\AccountActionInvalidException;
+use App\Domain\Exception\AccountNotFoundException;
 
 interface AccountStateMachineInterface
 {
     /**
      * @throws AccountActionInvalidException
+     * @throws AccountNotFoundException
      */
     public function apply(string $uuid, string $action): void;
 }
