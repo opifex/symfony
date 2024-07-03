4c2c1350078842b4e5f106f964f4dc64ffd1d874
Mykyta Zinchenko <opifex.ua@gmail.com>
2024-07-03 11:40:53 +0300

    Initial commit

---
+++ tests/Unit/KernelExceptionEventListenerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Application\EventListener\KernelExceptionEventListener;
+use App\Domain\Contract\PrivacyProtectorInterface;
+use Codeception\Test\Unit;
+use LogicException;
+use Override;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
+use ReflectionException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class KernelExceptionEventListenerTest extends Unit
+{
+    private KernelInterface&MockObject $kernel;
+    private LoggerInterface&MockObject $logger;
+    private NormalizerInterface&MockObject $normalizer;
+    private PrivacyProtectorInterface&MockObject $privacyProtector;
+
+    /**
+     * @throws Exception
+     */
+    #[Override]
+    protected function setUp(): void
+    {
+        $this->kernel = $this->createMock(originalClassName: KernelInterface::class);
+        $this->logger = $this->createMock(originalClassName: LoggerInterface::class);
+        $this->normalizer = $this->createMock(originalClassName: NormalizerInterface::class);
+        $this->privacyProtector = $this->createMock(originalClassName: PrivacyProtectorInterface::class);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     * @throws ReflectionException
+     */
+    public function testInvokePeriodicTask(): void
+    {
+        $event = new ExceptionEvent(
+            kernel: $this->kernel,
+            request: new Request(),
+            requestType: HttpKernelInterface::MAIN_REQUEST,
+            e: new LogicException(),
+        );
+
+        (new KernelExceptionEventListener($this->logger, $this->normalizer, $this->privacyProtector))($event);
+    }
+}
