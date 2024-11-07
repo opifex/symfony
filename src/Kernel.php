d17cf2135eff96671bac56a829507650cf4a087c
Mykyta Zinchenko <opifex.ua@gmail.com>
2024-11-07 19:39:51 +0200

    Initial commit

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+#[Exclude]
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
