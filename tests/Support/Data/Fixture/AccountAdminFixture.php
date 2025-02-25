fd7fce78e09b5dec5397de9f971bcee102717aa0
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-02-25 20:34:51 +0200

    Initial commit

--- tests/Support/Data/Fixture/AccountAdminFixture.php
+++ tests/Support/Data/Fixture/AccountAdminFixture.php
@@ -13,7 +13,6 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory as Faker;
 use Override;
-use Symfony\Component\PasswordHasher\Hasher\NativePasswordHasher;
 
 final class AccountAdminFixture extends Fixture implements FixtureInterface
 {
@@ -21,14 +20,12 @@ final class AccountAdminFixture extends Fixture implements FixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Faker::create();
-        $passwordHasher = new NativePasswordHasher();
-        $password = $passwordHasher->hash(plainPassword: 'password4#account');
 
         $account = new AccountEntity(
             uuid: '00000000-0000-6000-8000-000000000000',
             createdAt: new DateTimeImmutable(),
             email: $faker->unique()->bothify(string: 'admin@example.com'),
-            password: $password,
+            password: 'password4#account',
             locale: 'en_US',
             roles: [AccountRole::Admin->value],
             status: AccountStatus::Activated->value,
