13bdc6f517fedb87de20083ef4fde616c3b1c006
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-02-26 09:08:51 +0200

    Initial commit

---
+++ tests/Support/Extension/DisableCollector.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Support\Extension;
+
+use Codeception\Events;
+use Codeception\Extension;
+
+final class DisableCollector extends Extension
+{
+    /**
+     * @var array<string, string>
+     */
+    protected static array $events = [
+        Events::SUITE_BEFORE => 'disableCollector'
+    ];
+
+    public function disableCollector(): void
+    {
+        gc_disable();
+    }
+}
