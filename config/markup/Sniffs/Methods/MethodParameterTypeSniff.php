2d943f9f1bd5eac4afb2770b722de95d580ec926
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-03-06 16:38:13 +0200

    Initial commit

---
+++ config/markup/Sniffs/Methods/MethodParameterTypeSniff.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sniffs\Methods;
+
+use Override;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class MethodParameterTypeSniff implements Sniff
+{
+    #[Override]
+    public function process(File $phpcsFile, mixed $stackPtr): void
+    {
+        $methodName = $phpcsFile->getDeclarationName($stackPtr) ?? 'closure';
+        $methodParameters = $phpcsFile->getMethodParameters($stackPtr);
+
+        foreach ($methodParameters as $parameter) {
+            if ($parameter['type_hint'] === '') {
+                $phpcsFile->addError(
+                    error: 'The method "%s" has parameter %s without type hinting',
+                    stackPtr: $stackPtr,
+                    code: 'MethodParameterType',
+                    data: [$methodName, $parameter['name']],
+                );
+            }
+        }
+    }
+
+    #[Override]
+    public function register(): array
+    {
+        return [T_CLOSURE, T_FUNCTION];
+    }
+}
