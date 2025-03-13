58a5f7a01fb6a2e57b1ff4108969b6ded4f63926
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-03-13 13:13:39 +0200

    Initial commit

--- src/Infrastructure/Doctrine/Mapping/Default/AccountEntity.php
+++ src/Infrastructure/Doctrine/Mapping/Default/AccountEntity.php
@@ -6,38 +6,38 @@ namespace App\Infrastructure\Doctrine\Mapping\Default;
 
 use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\Mapping;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\DependencyInjection\Attribute\Exclude;
 
 #[Exclude]
-#[Mapping\Entity]
-#[Mapping\Table(name: 'account')]
+#[ORM\Entity]
+#[ORM\Table(name: 'account')]
 final class AccountEntity
 {
     /**
      * @param string[] $roles
      */
     public function __construct(
-        #[Mapping\Id]
-        #[Mapping\Column(name: 'uuid', type: Types::GUID)]
+        #[ORM\Id]
+        #[ORM\Column(name: 'uuid', type: Types::GUID)]
         public readonly string $uuid,
 
-        #[Mapping\Column(name: 'created_at', type: Types::DATETIME_IMMUTABLE, updatable: false)]
+        #[ORM\Column(name: 'created_at', type: Types::DATETIME_IMMUTABLE, updatable: false)]
         public readonly DateTimeImmutable $createdAt,
 
-        #[Mapping\Column(name: 'email', type: Types::STRING, unique: true, options: ['length' => 320])]
+        #[ORM\Column(name: 'email', type: Types::STRING, unique: true, options: ['length' => 320])]
         public readonly string $email,
 
-        #[Mapping\Column(name: 'password', type: Types::STRING, options: ['length' => 60])]
+        #[ORM\Column(name: 'password', type: Types::STRING, options: ['length' => 60])]
         public readonly string $password,
 
-        #[Mapping\Column(name: 'locale', type: Types::STRING, options: ['length' => 5])]
+        #[ORM\Column(name: 'locale', type: Types::STRING, options: ['length' => 5])]
         public readonly string $locale,
 
-        #[Mapping\Column(name: 'roles', type: Types::JSON)]
+        #[ORM\Column(name: 'roles', type: Types::JSON)]
         public readonly array $roles,
 
-        #[Mapping\Column(name: 'status', type: Types::STRING, options: ['length' => 24])]
+        #[ORM\Column(name: 'status', type: Types::STRING, options: ['length' => 24])]
         public readonly string $status,
     ) {
     }
