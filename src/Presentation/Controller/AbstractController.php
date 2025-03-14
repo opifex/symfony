a9cd0f74d135fc276e7ec3e4fd1aef16670a253b
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-03-14 16:10:30 +0200

    Initial commit

--- src/Presentation/Controller/AbstractController.php
+++ src/Presentation/Controller/AbstractController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Presentation\Controller;
 
-use Symfony\Component\Messenger\Exception\LogicException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -19,19 +18,7 @@ abstract class AbstractController
 
     protected function handleMessage(object $message): mixed
     {
-        $envelope = $this->messageBus->dispatch($message);
-        $handledStamps = $envelope->all(stampFqcn: HandledStamp::class);
-
-        if (count($handledStamps) !== 1) {
-            throw new LogicException(
-                sprintf(
-                    'Message of type "%s" was handled multiple times, but only one handler is expected.',
-                    get_debug_type($envelope->getMessage()),
-                ),
-            );
-        }
-
-        return $handledStamps[0]->getResult();
+        return $this->messageBus->dispatch($message)->last(stampFqcn: HandledStamp::class)?->getResult();
     }
 
     protected function normalizeResult(mixed $data): mixed
