2a9235e5cbed73c672616bef41a1f9dcef3db6be
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-08-05 11:01:05 +0300

    Initial commit

--- src/Domain/Contract/Authorization/AuthorizationTokenManagerInterface.php
+++ src/Domain/Contract/Authorization/AuthorizationTokenManagerInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Contract\Authorization;
 
+use App\Domain\Exception\Authorization\AuthorizationForbiddenException;
 use App\Domain\Exception\Authorization\AuthorizationRequiredException;
 use App\Domain\Model\Role;
 
@@ -15,6 +16,7 @@ interface AuthorizationTokenManagerInterface
     public function getUserIdentifier(): string;
 
     /**
+     * @throws AuthorizationForbiddenException
      * @throws AuthorizationRequiredException
      */
     public function checkUserPermission(Role $role, mixed $subject = null): void;
