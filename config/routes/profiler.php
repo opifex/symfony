74076380c7c22ff821cbf78e212e0400e6235305
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-09-17 14:23:11 +0300

    Initial commit

--- config/routes/profiler.php
+++ config/routes/profiler.php
@@ -6,10 +6,10 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $configurator): void {
     if ($configurator->env() === 'dev') {
-        $configurator->import(resource: '@WebProfilerBundle/Resources/config/routing/wdt.xml')
+        $configurator->import(resource: '@WebProfilerBundle/Resources/config/routing/wdt.php')
             ->prefix(prefix: '/_wdt');
 
-        $configurator->import(resource: '@WebProfilerBundle/Resources/config/routing/profiler.xml')
+        $configurator->import(resource: '@WebProfilerBundle/Resources/config/routing/profiler.php')
             ->prefix(prefix: '/_profiler');
     }
 };
--- config/routes/webhook.php
+++ config/routes/webhook.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $configurator): void {
-    $configurator->import(resource: '@FrameworkBundle/Resources/config/routing/webhook.php')
-        ->namePrefix(namePrefix: 'app')
-        ->prefix(prefix: '/webhook');
+    $configurator->add(name: 'app_webhook_api', path: '/webhook/{type}')
+        ->defaults(['_controller' => 'webhook.controller::handle']);
 };
