13959d0228e26d6c659d90f4fd4382b45fb41ebe
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-10-02 15:35:11 +0300

    Initial commit

---
+++ tests/Unit/AuthenticationRateLimiterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Application\Service\AuthenticationRateLimiter;
+use Override;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\RateLimiter\LimiterInterface;
+use Symfony\Component\RateLimiter\RateLimit;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
+
+final class AuthenticationRateLimiterTest extends TestCase
+{
+    private RateLimiterFactoryInterface&MockObject $rateLimiterFactory;
+
+    #[Override]
+    protected function setUp(): void
+    {
+        $this->rateLimiterFactory = $this->createMock(type: RateLimiterFactoryInterface::class);
+    }
+
+    public function testIsAcceptedThrowsExceptionForThrottlingEmail(): void
+    {
+        $authenticationRateLimiter = new AuthenticationRateLimiter($this->rateLimiterFactory);
+
+        $rateLimit = $this->createConfiguredMock(
+            type: RateLimit::class,
+            configuration: ['isAccepted' => true],
+        );
+
+        $limiter = $this->createConfiguredMock(
+            type: LimiterInterface::class,
+            configuration: ['consume' => $rateLimit],
+        );
+
+        $this->rateLimiterFactory
+            ->expects($this->once())
+            ->method(constraint: 'create')
+            ->willReturn($limiter);
+
+        $isAccepted = $authenticationRateLimiter->isAccepted(
+            emailAddress: 'admin@example.com',
+        );
+
+        $this->assertTrue($isAccepted);
+    }
+}
