016b7e9d0c5daa9be493ec8c1301ee098b279518
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2025-11-04 09:21:21 +0200

    Initial commit

---
+++ config/packages/lock.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Config\FrameworkConfig;
+
+return static function (FrameworkConfig $framework): void {
+    $framework->lock(value: '%env(LOCK_DSN)%');
+};
