5270b82dbd7553138080e9eebc77fda33351c98f
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2026-02-13 11:40:27 +0200

    Initial commit

--- tests/Unit/RequestNormalizerTest.php
+++ tests/Unit/RequestNormalizerTest.php
@@ -63,11 +63,10 @@ final class RequestNormalizerTest extends TestCase
     public static function requestDataProvider(): iterable
     {
         yield 'string remains string' => ['value' => 'string', 'expected' => 'string'];
-        yield 'array with string number to int' => ['value' => ['array' => '100'], 'expected' => ['array' => 100]];
+        yield 'string number remains string' => ['value' => '100', 'expected' => '100'];
+        yield 'array remains array' => ['value' => ['array' => 100], 'expected' => ['array' => 100]];
         yield 'integer remains integer' => ['value' => 100, 'expected' => 100];
-        yield 'string number to integer' => ['value' => '100', 'expected' => 100];
         yield 'float remains float' => ['value' => 100.56, 'expected' => 100.56];
-        yield 'string float to float' => ['value' => '100.56', 'expected' => 100.56];
         yield 'null remains null' => ['value' => null, 'expected' => null];
         yield 'string "null" to null' => ['value' => 'null', 'expected' => null];
         yield 'boolean true remains true' => ['value' => true, 'expected' => true];
