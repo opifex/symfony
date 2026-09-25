49bceb73abbe7f0dd15a142eeda044618fdc93d9
Mykyta Zinchenko <mykyta.zinchenko@icloud.com>
2026-09-25 14:30:20 +0300

    Initial commit

--- src/Domain/Foundation/ImmutableCloneTrait.php
+++ src/Domain/Foundation/ImmutableCloneTrait.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Domain\Foundation;
 
+use NoDiscard;
+
 trait ImmutableCloneTrait
 {
     /**
      * @param array<string, mixed> $properties
      */
+    #[NoDiscard]
     private function withFields(array $properties): static
     {
         return clone($this, $properties);
